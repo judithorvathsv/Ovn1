ea88250048da21c4c8f7b32dcf04e05390effd00
MyPC <MyPC@DESKTOP-2CGKRM8>
2021-06-15 09:48:08 +0200

    Add project files.

---
+++ Ovn1/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Ovn1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
